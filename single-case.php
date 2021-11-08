cbe2dee2689c9778d09fbb981e8ef8c71a7ef3be
j-polman <j.polman@webdesignhq.nl>
2021-11-08 13:06:32 +0100

    acf case fields

--- single-case.php
+++ single-case.php
@@ -2,12 +2,13 @@
 /* Template Name: Case */
 
 get_header();
+
 ?>
 <div id="intro">
     <div class="row">
         <div class="container-lg">
-            <h2 class="post__subtitle">Post-COVID-19 Capaciteitsplanner DicaPlan</h2>
-            <span><?php echo the_excerpt(); ?></span>
+            <h2 class="post__subtitle"><?php echo get_field( "case_titel"); ?></h2>
+            <span><?php echo get_field( "case_inleiding"); ?></span>
         </div>
     </div>
 </div>
@@ -31,28 +32,28 @@ get_header();
 
 <div id="quote" class="my-5">
     <div class="container-lg">
-        <p>“Margit en Judith hebben in een korte tijd een zeer waardevolle planner voor ons ontwikkeld. Hun brede kennis, flexibiliteit, toegankelijkheid en kritische houding hebben ervoor gezorgd dat we een relevante tool tot onze beschikking hebben, die voldoet aan de wensen van de eindgebruikers en duurzaam is voor de toekomst.”</p>
+        <p>“<?php echo get_field( "case_quote"); ?>”</p>
     </div>
 </div>
 
 <div id="content__1" class="my-5 case__content">
     <div class="container-lg">
         <span>1. <h2>Uitdaging</h2></span>
-        <p>Door de uitbraak van COVID-19 in Nederland vindt er vertraging in diagnostiek en/of uitstel van behandelingen plaats in de zorg. De zorg die nu wordt uitgesteld, leidt over een paar maanden tot een extra hoeveelheid benodigde zorg en heeft een directe weerslag op IC-, OK- en opnamecapaciteit in de ziekenhuizen. Om de reguliere zorg weer op te starten, is het noodzakelijk om inzichtelijk te maken wat de benodigde capaciteit is.DicaPlan is een digitale tool die de huidige achterstand in IC-, OK- en opnamecapaciteit van ziekenhuizen met betrekking tot uitgestelde reguliere zorg inzichtelijk maakt. In deze editie van Allchiefs Klantcase laten we zien hoe onze Data experts Margit & Judithsamen met DICA dit project hebben doorlopen.</p>
+        <p><?php echo get_field( "uitdaging_content" ); ?></p>
     </div>
 </div>
 
 <div id="content__2" class="my-5 case__content">
     <div class="container-lg">
         <span>2. <h2>Aanpak</h2></span>
-        <p>Door de uitbraak van COVID-19 in Nederland vindt er vertraging in diagnostiek en/of uitstel van behandelingen plaats in de zorg. De zorg die nu wordt uitgesteld, leidt over een paar maanden tot een extra hoeveelheid benodigde zorg en heeft een directe weerslag op IC-, OK- en opnamecapaciteit in de ziekenhuizen. Om de reguliere zorg weer op te starten, is het noodzakelijk om inzichtelijk te maken wat de benodigde capaciteit is.DicaPlan is een digitale tool die de huidige achterstand in IC-, OK- en opnamecapaciteit van ziekenhuizen met betrekking tot uitgestelde reguliere zorg inzichtelijk maakt. In deze editie van Allchiefs Klantcase laten we zien hoe onze Data experts Margit & Judithsamen met DICA dit project hebben doorlopen.</p>
+        <p><?php echo get_field( "uitdaging_content_2"); ?></p>
     </div>
 </div>
 
 <div id="content__3" class="my-5 case__content">
     <div class="container-lg">
         <span>3. <h2>Resultaat</h2></span>
-        <p>Het resultaat is een interactieve capaciteitsplanner met duidelijke functies voor haar gebruikers. Dit stelt hen in staat om onderbouwde beslissingen te nemen met betrekking tot het opstarten en inplannen van reguliere zorg op verschillende niveaus. De capaciteitsplanner is gebruiksvriendelijk en dankzij de handleiding kan de doelgroep deze zelfstandig gebruiken. De planner kan in de toekomst gemakkelijk worden uitgebreid voor extra toepassingen en is daarmee een duurzame oplossing. Zie onderaan deze pagina het dashboard van de Non COVID-19 capaciteitsplanner.</p>
+        <p><?php echo get_field( "uitdaging_content_3"); ?></p>
     </div>
 </div>
 
