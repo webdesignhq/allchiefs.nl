5200932f631f274d8e0230637f87ed8ee1e4ff3b
Adinda Webdesignhq <a.knevel@webdesignhq.nl>
2021-11-17 10:37:09 +0100

    fix excellences on chief page

--- about-us.php
+++ about-us.php
@@ -78,6 +78,15 @@ get_header();
 										}
 									}
 							?>
+							<?php $categories = get_the_category();
+									$separator = ' ';
+									$output = '';
+									if ( ! empty( $categories ) ) {
+										foreach( $categories as $category ) {
+											$output .= '<a class="btn btn-primary" href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+									}
+									echo trim( $output, $separator );
+							}?>
                         </div>
                     </div>
 
