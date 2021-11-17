7a9ec0d3af4830f1d3a1868e0b18603f58853df1
Adinda Webdesignhq <a.knevel@webdesignhq.nl>
2021-11-17 12:27:23 +0100

    About us changes

--- about-us.php
+++ about-us.php
@@ -1,12 +1,8 @@
 <?php
-/* 
-Template Name: About us
-*/
+/*  Template Name: About us */
+get_header(); ?>
 
-get_header();
-?>
-<div id="about-us" class="py-5"  style="background: linear-gradient(
--90deg, var(--white-color) 40%, var(--sand-25) 15%, var(--sand-25) 55%);">
+<div id="about-us" class="py-5"  style="background: linear-gradient(-90deg, var(--white-color) 40%, var(--sand-25) 15%, var(--sand-25) 55%);">
     <div class="container-xxl">
         <div class="row py-5">
         <div class="col-5 d-flex flex-column justify-content-center content-height" >
@@ -23,51 +19,44 @@ get_header();
 <div id="section-2">
     <div class="container-xxl">
         <div class="row">
-                <div class="col-12 d-flex flex-column">
-                    <h2 class="text-center">Onze kernwaarden</h2>
-                </div>
-                 <div class="col-12 d-flex flex-row d-flex">
-                     <?php if (have_posts()) : ?>     
-		        <?php  
+            <div class="col-12 d-flex flex-column">
+                <h2 class="text-center">Onze kernwaarden</h2>
+            </div>
+            <div class="col-12 d-flex flex-row d-flex">
+				<?php
 					$args = array(
-						'post_type'      => 'kernwaarde',
+					'post-type' => 'kernwaarde',
 						'posts_per_page' => 4,
 					);
 
 					$loop = new WP_Query( $args );
 
 					while ( $loop->have_posts() ) : $loop->the_post(); ?>
-					<?php $img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
 					<?php global $post; ?>
-
-                    <div class="flex-column d-flex col-3">
-						<img class="post-image" src="<?php echo $img[0]; ?>" style="width: 100%; height: 200px; object-fit: cover;"/>
-						<h3><?php echo $post->post_title; ?></h3>
-						<p><?php echo $post->post_content; ?></p>
-                    </div>
-
-                    <?php endwhile; ?>
-
-                    <?php else: ?>
-                        <p>Sorry, er zijn geen kernwaarden gevonden<p>
-                    <?php endif 
-
-                    // wp_reset_query();
-                    ?>
-                    
-                </div>
+					
+					<?php $img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
+					<div class="position-relative post-container my-lg-5 my-2">
+						<div class="post clickable me-lg-4">
+							<img class="post-image" src="<?php echo $img[0]; ?>" style="width: 100%; height: 200px; object-fit: cover;"/>
+							<p class="post-title mt-4"><?php echo get_the_title(); ?></p>
+							<p class="post-exerpt"><?php echo the_content(); ?></p>
+							<a class="d-none" href="<?php echo get_permalink();?>">Lees meer</a>
+						</div>
+					</div> 
+					<?php endwhile; ?>
             </div>
+        </div>
     </div>
 </div>
+
 
 <div id="section-3" style="background-color: var(--sand-25); padding-bottom: 110px;">
     <div class="container-xxl">
         <div class="row">
-                <div class="col-12 d-flex flex-column">
-                    <h2>De reis van Allchiefs</h2>
-                </div>
+            <div class="col-12 d-flex flex-column">
+                <h2>De reis van Allchiefs</h2>
+            </div>
                  <div class="slidertrip">
-                 
 		        <?php  
 					$args = array(
 						'post_type'      => 'kernwaarde',
@@ -86,9 +75,9 @@ get_header();
 						<h3><?php echo $post->post_title; ?></h3>
 						<p><?php echo $post->post_content; ?></p>
                     </div>
-                    <?php endwhile; ?>
-                </div>
-            </div>
+                <?php endwhile; ?>
+			</div>
+        </div>
     </div>
 </div>
 
@@ -96,8 +85,7 @@ get_header();
     <div class="container-xxl">
         <div class="row">
             <h2>Chiefs</h2>
-            <div class="col-12 d-flex flex-wrap justify-content-between position-relative">
-            <?php if (have_posts()) : ?>     
+            <div class="col-12 d-flex flex-wrap justify-content-between position-relative">   
 		        <?php  
 					$args = array(
 						'post_type'      => 'chief',
@@ -138,15 +126,7 @@ get_header();
 							}?>
                         </div>
                     </div>
-
                     <?php endwhile; ?>
-
-                    <?php else: ?>
-                        <p>Sorry, er zijn geen chiefs gevonden<p>
-                    <?php endif 
-
-                    // wp_reset_query();
-                    ?>
             </div>
         </div>
     </div> 
